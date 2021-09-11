4b59d3dec8fdc1fd8c59542d8394e9ee28f2ab16
Kyle <kylerobertduncan@gmail.com>
2021-09-11 12:30:57 -0400

    Need to figure out how to loop API calls and wait for response in each loop

--- src/RandomSynonyms.js
+++ src/RandomSynonyms.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const RandomSynonyms = (props) => {
   const { newStory } = props;
   // const [ synonym, setSynonym ] = useState("");
-  const [ synonymSearch, setSynonymSearch ] = useState("");
+  // const [ synonymSearch, setSynonymSearch ] = useState("");
   const [ randomStory, setRandomStory ] = useState({});
 
   // useEffect(() => {
@@ -29,20 +29,36 @@ const RandomSynonyms = (props) => {
   //   }
   // })
 
-  const getRandomSynonym = (searchTerm) => {
-    const url = new URL('https://api.datamuse.com/words');
-    url.search = new URLSearchParams({
-      ml: searchTerm, // ml = "means like"
-      max: 1
-    });
-    fetch(url)
-      .then(response => response.json())
-      .then((jsonResponse) => {
-        // console.log(jsonResponse[0].word);
-        const synonym = jsonResponse[0].word
-        console.log(synonym);
-        return(synonym);
-      });
+  const getRandomSynonyms = (newStory) => {
+    for (let element in newStory) {
+      // split to take apart each element
+      const wordArray = newStory[element].split(" ")
+
+      // pass each word through datamuse
+      wordArray.map( (word) => {
+        let randomArray = [];
+        const url = new URL('https://api.datamuse.com/words');
+        url.search = new URLSearchParams({
+          ml: word, // ml = "means like"
+          max: 1
+        });
+        fetch(url)
+          .then(response => response.json())
+          .then((jsonResponse) => {
+            // console.log(jsonResponse[0].word);
+            randomArray = [...randomArray, jsonResponse[0].word]
+            // setRandomStory([element].randomArray);
+            // console.log(randomStory);
+            // const synonym = jsonResponse[0].word
+            // console.log(synonym);
+            // return(synonym);
+          })
+          .then(() => {
+            console.log(randomArray);
+          })
+
+      })
+    }
   }
 
   // const getRandomSynonym = (searchTerm) => {
@@ -50,33 +66,33 @@ const RandomSynonyms = (props) => {
   //   return(synonym);
   // }
 
-  const handleGetSynonyms = () => {
-    // console.log(props.newStory);
-    let newRandomStory = {};
-    // console.log(newRandomStory);
-    for (const element in newStory) {
-      // console.log(element);
-      // split to take apart each element
-      const wordArray = newStory[element].split(" ");
+  // const handleGetSynonyms = () => {
+  //   // console.log(props.newStory);
+  //   let newRandomStory = "";
+  //   // console.log(newRandomStory);
+  //   for (const element in newStory) {
+  //     // console.log(element);
+  //     // split to take apart each element
+  //     const wordArray = newStory[element].split(" ");
 
-      // pass each word through datamuse
-      const randomArray = wordArray.map( (word) => {
-        getRandomSynonym(word);
-        // compare to 'likely to follow' word list ??
-      })
-      console.log(randomArray);
-      // this returns all undefined, before the getRandomSynonym loop can run/resolve ??
+  //     // pass each word through datamuse
+  //     const randomArray = wordArray.map( (word) => {
+  //       getRandomSynonym(word);
+  //       // compare to 'likely to follow' word list ??
+  //     })
+  //     console.log(randomArray);
+  //     // this returns all undefined, before the getRandomSynonym loop can run/resolve ??
 
-      // reconstruct back into newStory object
-      newRandomStory[element] = randomArray
-      // .join(" ");      
-      // console.log(newRandomStory);
-    }
-    // console.log(synonym);
-  }
+  //     // reconstruct back into newStory object
+  //     newRandomStory[element] = randomArray
+  //     // .join(" ");      
+  //     // console.log(newRandomStory);
+  //   }
+  //   // console.log(synonym);
+  // }
 
   return (
-    <button onClick={handleGetSynonyms}>Gimme extra rando stuff!</button>
+    <button onClick={() => getRandomSynonyms(newStory)}>Gimme extra rando stuff!</button>
   )
 }
 
