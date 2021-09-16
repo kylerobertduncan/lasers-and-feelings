5a826fb2634b777abb6a5bdd04ddc455fb51a158
Kyle <kylerobertduncan@gmail.com>
2021-09-16 15:23:06 -0400

    Added filter for joining words, ideas for stretch goals

--- README.md
+++ README.md
@@ -9,24 +9,26 @@ Available at http://www.onesevendesign.com/laserfeelings/
 - Divided by adventure sentance structure
 
 ### Map each element of the adventure sentance and randomly select one option
-- Make an app that randomly selects one item of an array
+x Make an app that randomly selects one item of an array
   - check for "/" character, choose one (or offer choice?)
 - Add each choice to a "sentance" array/string
 
 ### Display the complete sentance on the page
 - Join the components into a complete string
-- Print to the page 
+x Print to the page 
 
 ### Randomly generate alternatives with Datamuse
-- connect Datamuse API
-- break sentance elements into individual words
-- generate synonyms via Datamuse
-- randomly select a synonym
+x connect Datamuse API
+x break sentance elements into individual words
+x generate synonyms via Datamuse
+x randomly select a synonym
   - compare against "words that commonly follow" for legibility
-- reconstruct sentance element
+x reconstruct sentance element
 
 ## stretch goals
 - automate capitalization and plurals
-- random character generator
+- click individual words to change just one
+- random character generator (names? https://randommer.io/pet-names)
 - random ship generator
+- drop down selection from original elements for customization
 - save data to firebase
--- src/LoopAPI.js
+++ src/LoopAPI.js
@@ -3,8 +3,8 @@ import selectRandom from "./selectRandom";
 
 const LoopAPI = (props) => {
   const { currentStory } = props;
-
   const [ newStory, setNewStory ] = useState({});
+  const joiningWords = ['a', 'the', 'in', 'with']
   
   const replaceCurrentStory = async () => {
     
@@ -18,24 +18,35 @@ const LoopAPI = (props) => {
       const newElementArray = [];
 
       for (const wordToReplace of elementArray) {
-        // add code to skip 'a', 'the', 'in' and 'with'
-
-        // API fetch setup
-        const url = new URL('https://api.datamuse.com/words');
-        url.search = new URLSearchParams({
-          ml: wordToReplace, // ml = "means like"
-          max: 10
+        // skip 'a', 'the', 'in' and 'with'
+        let skipJoiningWord = false;
+        joiningWords.map( (word) => {
+          if (word === wordToReplace.toLowerCase()) {
+            skipJoiningWord = true;
+          }
         });
-
-        // call the API for 10 random synonyms
-        const response = await fetch(url);
-        // turn the synonyms in a json array
-        const synonymsArray = await response.json();
-          // add code to compare random options to 'likely to follow'
-        // choose a random word from the json array
-        const newWord = await selectRandom(synonymsArray).word;
-        // add that word to a replacement array
-        newElementArray.push(newWord);
+        // check if wordToReplace is a joining word
+        if (skipJoiningWord) {
+          // if true, add to new array
+          newElementArray.push(wordToReplace);
+        // otherwise generat a synonym
+        } else {
+          // API fetch setup
+          const url = new URL('https://api.datamuse.com/words');
+          url.search = new URLSearchParams({
+            ml: wordToReplace, // ml = "means like"
+            // max: 10
+          });
+          // call the API for 10 random synonyms
+          const response = await fetch(url);
+          // turn the synonyms in a json array
+          const synonymsArray = await response.json();
+            // add code to compare random options to 'likely to follow'
+          // choose a random word from the json array
+          const newWord = await selectRandom(synonymsArray).word;
+          // add that word to a replacement array
+          newElementArray.push(newWord);
+        }
       }
 
       // turn the replacement array into a string
@@ -43,8 +54,8 @@ const LoopAPI = (props) => {
       // add the new string to the relevant story element
       newStoryObject[element] = newStoryElement;
     }
-    console.log(newStoryObject);
-    setNewStory(newStoryObject)
+    // set the generated story in state
+    setNewStory(newStoryObject);
   }
 
 
@@ -52,7 +63,7 @@ const LoopAPI = (props) => {
     <div>
       <button onClick={replaceCurrentStory}>Generate Random Story</button>
       <p>
-        <span className="element">{newStory.threat}</span> want(s) to <span className="element">{newStory.want}</span> the <span className="element">{newStory.target}</span> which will <span className="element">{newStory.result}</span>!
+        <span className="element threat">{newStory.threat}</span> want(s) to <span className="element">{newStory.want}</span> the <span className="element">{newStory.target}</span> which will <span className="element">{newStory.result}</span>!
       </p>
     </div>
   );
